b85fb0ebdad6802fec244727b807b3c8c779b849
DevinPierre <46579118+DevinPierre@users.noreply.github.com>
2019-01-17 15:20:33 -0500

    initial commit

---
+++ Gravity Games/Assets/Devin_Pierre/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        gameObject.transform.position = new Vector3(0, 5, 0);
+
+    }
+
+    private void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}    
