b60bc25f20943c17cbfb080123edd8d05da730f2
Jetrumble1977 <46725879+Jetrumble1977@users.noreply.github.com>
2019-01-24 15:17:17 -0500

    updated

---
+++ Gravity Games/Assets/BallReset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallReset : MonoBehaviour
+{
+    [SerializeField] private Transform otherObject;
+    private Vector3 respawnPoint;
+
+    void Start()
+    {
+        respawnPoint = otherObject.transform.position;
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == otherObject.gameObject.name)
+        {
+            otherObject.transform.position = respawnPoint;
+        }
+    }
+}
