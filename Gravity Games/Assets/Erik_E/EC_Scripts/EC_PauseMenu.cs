7ee64f3193523b95d89b885e2741f0734edc766b
ErikCortez713 <46726190+ErikCortez713@users.noreply.github.com>
2019-01-24 14:18:24 -0500

    Created Pause Menu in Test Scene

---
+++ Gravity Games/Assets/Erik_E/EC_Scripts/EC_PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EC_PauseMenu : MonoBehaviour
+{
+
+    bool paused = false;
+
+    public GameObject canvas;
+    bool showCanvas = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            paused = togglePause();
+            showCanvas = toggleShowCanvas();
+        }
+    }
+
+    bool togglePause()
+    {
+        if(Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+
+    bool toggleShowCanvas()
+    {
+        if(showCanvas == false)
+        {
+            canvas.SetActive(true);
+            return (true);
+        }
+        else
+        {
+            canvas.SetActive(false);
+            return (false);
+        }
+    }
+
+}
