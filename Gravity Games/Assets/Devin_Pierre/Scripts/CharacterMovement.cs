0944414456888f87be8d13f1e8e75095f8b26924
DevinPierre <46579118+DevinPierre@users.noreply.github.com>
2019-01-27 20:05:36 -0500

    fixed character spawn

--- Gravity Games/Assets/Devin_Pierre/Scripts/CharacterMovement.cs
+++ Gravity Games/Assets/Devin_Pierre/Scripts/CharacterMovement.cs
@@ -7,17 +7,17 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-   
+
 
     private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
 
     private void Start()
     {
-       
+
         controller = GetComponent<CharacterController>();
 
-       
+
 
     }
 
@@ -40,5 +40,5 @@ public class CharacterMovement : MonoBehaviour
         controller.Move(moveDirection * Time.deltaTime);
     }
 
-    
-}    
+
+}
