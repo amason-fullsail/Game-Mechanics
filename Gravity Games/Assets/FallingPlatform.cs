0888a8538a1809611ca0fc8fd0ef44cb10b9120d
Alden Mason <amason-fullsail@users.noreply.github.com>
2019-01-27 20:24:32 -0500

    level push

---
+++ Gravity Games/Assets/FallingPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float fallTimer = 1.5f;
+
+    void OnCollisionEnter(Collision collidedWithThis)
+    {
+        if (collidedWithThis.gameObject.tag == "Player")
+        {
+            StartCoroutine(FallAfterDelay());
+        }
+    }
+    
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallTimer);
+        GetComponent<Rigidbody>().isKinematic = false;
+        Debug.Log("big anime titty");
+    }
+}
