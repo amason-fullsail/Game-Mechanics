1a3b1e3396ddf3188f4b5d430648fc83dba7b1f1
Alden Mason <amason-fullsail@users.noreply.github.com>
2019-01-29 19:34:10 -0500

    yee (and i can't stress this enough) haw

---
+++ Gravity Games/Assets/Alden/Scripts/AM_RestartScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AM_RestartScene : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform == Player)
+        {
+            Debug.Log("deadified");
+            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+        }
+    }
+}
